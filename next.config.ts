073fb4ac20e867327a89beaab02cff52e25bd139
seungyeon.baek <qorbaxk97@gmail.com>
2025-04-07 14:28:17 +0900

    init : 레포지토리명 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const repositoryName = "findsy";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: `/${repositoryName}`,
+  assetPrefix: `/${repositoryName}`,
 };
 
 export default nextConfig;
