78e3f6a182ddd2900ed023da0c9ef31b87a4c612
seungyeon.baek <qorbaxk97@gmail.com>
2025-04-09 15:40:02 +0900

    style : 뮤직플레이어 위치 변경

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -52,7 +52,7 @@ const MusicPlayer = () => {
   }, []);
 
   return (
-    <div className="fixed bottom-4 right-4 z-50">
+    <div className="fixed bottom-2 right-2 z-50">
       {/* 돌아가는 LP 이미지 */}
       <Tooltip text={isPlaying ? "음악 정지하기" : "음악 재생하기"}>
         <button
